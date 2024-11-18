4b6544b70353124e07ed4e5208fec1506800a2c9
bradenmweight <braden.m.weight@gmail.com>
2024-11-18 15:58:46 -0500

    11/18/2024

--- main.py
+++ main.py
@@ -6,10 +6,9 @@ from pyscf import gto
 
 
 def get_Globals():
-    global L, NPW, NELECT
+    global L, NPW
     L      = 4.0
-    NPW    = 3 # Number of plane-waves per dimension -- odd
-    NELECT = 2
+    NPW    = 11 # Number of plane-waves per dimension -- odd
 
     # Molecular System
     global coords, charges, n_elec_alpha, n_elec_beta
@@ -48,41 +47,10 @@ def get_T_e( k1 ):
     return 0.500 * np.dot( k1, k1 )
 
 @njit
-def get_two_electron_integral( k1, k2, k3, k4 ):
-    k34   = k3 - k4
-    if ( (k34 == k2 - k1).all() and np.dot( k34, k34 ) != 0.0 ):
-        return 4 * np.pi / np.dot( k34, k34 )
-    else:
-        return 0.0 + 0.0j
-
-#@njit
 def get_1RDM( C, k3i, k4i ):
     return np.sum( C[k3i,:].conj() * C[k4i,:] )
 
 @njit
-def get_V_ee_OLD( k1i, k2i, k1, k2, C, kGRID ):
-    """
-    Chemists' Notation:
-    J = [ 12 | 34 ] D_{34}
-    K = [ 13 | 24 ] D_{34}
-    Condition: k3 - k4 = k2 - k1 --> k3 = k2 - k1 + k4
-    """
-    V_ee = 0.0 + 0.0j
-    for k4i, k4 in enumerate(kGRID):
-        k3    = k2 - k1 + k4
-        k3i   = k3 / 2 / np.pi * L + NPW // 2
-        k3i   = int( k3i[0] * NPW**2 + k3i[1] * NPW + k3i[2] ) #- 1
-        if ( k3i < 0 or k3i >= NPW**3 ):
-            continue
-        #print( k1i, k2i, k3i, k4i )
-        J     = get_two_electron_integral( k1, k2, k3, k4 )
-        K     = get_two_electron_integral( k1, k3, k2, k4 )
-        D34   = get_1RDM( C, k3i, k4i )
-        V_ee += (J - 0.5 * K) * D34
-
-    return V_ee
-
-#@njit
 def get_V_ee( k1i, k2i, k1, k2, C, kGRID ):
     """
     Chemists' Notation:
@@ -97,45 +65,60 @@ def get_V_ee( k1i, k2i, k1, k2, C, kGRID ):
     ### Coulomb Term ###
     k1i  = np.array([k1i // NPW**2, k1i // NPW % NPW, k1i % NPW])
     k2i  = np.array([k2i // NPW**2, k2i // NPW % NPW, k2i % NPW])
+    k21  = k2 - k1
     k34i = k2i - k1i
+
+    '''
+    if k34i < 0:
+        for k3i from 0 to NPW + k34i
+            k4i = k3i - k34i
+    if k34i > 0:
+        for k4i from 0 to NPW - k34i
+            k3i = k4i + k34i
+        for k3i from k34i to NPW
+            k4i = k3i - k34i
+
+    for k3i from k34i * (sign(k34i) + 1)/2 to NPW + k34i * (1 - sign(k34i))/2
+        k4i = k3i - k34i
+    '''
+
+
+
     if ( (k34i <= -NPW).any() or (k34i >= NPW).any() ):
         pass
     else:
         FACTOR_COUL =  4 * np.pi / np.dot( k2 - k1, k2 - k1 )
-        for nx in range( NPW-np.abs(k34i[0]) ):
-            for ny in range( NPW-np.abs(k34i[1]) ):
-                for nz in range( NPW-np.abs(k34i[2]) ):
-                    k4i = np.array([nx,ny,nz])
-                    k3i = k4i + k34i
+        for nx in range( k34i[0] * (np.sign(k34i[0]) + 1)//2 , NPW + k34i[0] * (1 - np.sign(k34i[0]))//2):
+            for ny in range( k34i[1] * (np.sign(k34i[1]) + 1)//2 , NPW + k34i[1] * (1 - np.sign(k34i[1]))//2 ):
+                for nz in range(k34i[2] * (np.sign(k34i[2]) + 1)//2 , NPW + k34i[2] * (1 - np.sign(k34i[2]))//2 ):
+                    k3i = np.array([nx,ny,nz])
+                    k4i = k3i - k34i
                     D34 = get_1RDM( C, k3i[0] * NPW**2 + k3i[1] * NPW + k3i[2], k4i[0] * NPW**2 + k4i[1] * NPW + k4i[2] )
-                    if ( np.sign(k34i[0]) * np.sign(k34i[1]) * np.sign(k34i[2]) < 0  ):
-                        D34 = np.conj(D34)
+                    # if ( np.sign(k34i[0]) * np.sign(k34i[1]) * np.sign(k34i[2]) < 0  ):
+                    #     D34 = np.conj(D34)
                     V_ee_J += FACTOR_COUL * D34
 
-
-
-
     # ### Exchange Term ###
-    # k34i = k2i + k1i
-
-    # if (k34i >= 2 * NPW).any() :
-    #     pass
-    # else:
-    #     nx_range = range( k34i[0] - NPW * (k34i[0]//NPW), NPW * (k34i[0]//NPW) )
-    #     ny_range = range( k34i[1] - NPW * (k34i[1]//NPW), NPW * (k34i[1]//NPW) )
-    #     nz_range = range( k34i[2] - NPW * (k34i[2]//NPW), NPW * (k34i[2]//NPW) )
-    #     for nx in nx_range :
-    #         for ny in ny_range :
-    #             for nz in nz_range :
-    #                 k4i = np.array([nx,ny,nz])
-    #                 k3i = k34i - k4i
-    #                 k4  = kGRID[k4i[0],k4i[1],k4i[2]]
-    #                 D34 = get_1RDM( C, k3i[0] * NPW**2 + k3i[1] * NPW + k3i[2], k4i[0] * NPW**2 + k4i[1] * NPW + k4i[2] )
-    #                 V_ee_K += 4 * np.pi * D34 / np.dot( k2 - k4, k2 - k4 )
-
-    return V_ee_J
-    #return V_ee_J - 0.5 * V_ee_K
-    #return V_ee_K
+    k34i = k2i + k1i
+    if ( (k34i >= 2*NPW).any()):
+        pass
+    else:
+        for nx in range( (2*NPW - k34i[0]) * (k34i[0] > NPW) , NPW * (k34i[0] >= NPW) + (1 + k34i[0]) * (NPW > k34i[0])):
+            for ny in range(  (2*NPW - k34i[1]) * (k34i[1] > NPW) , NPW * (k34i[1] >= NPW) + (1 + k34i[1]) * (NPW > k34i[1]) ):
+                for nz in range( (2*NPW - k34i[2]) * (k34i[2] > NPW) , NPW * (k34i[2] >= NPW) + (1 + k34i[2]) * (NPW > k34i[2]) ):
+                    
+                    k3i = np.array([nx,ny,nz])
+                    k4i = k34i - k3i
+                    if ( (k4i < 0).any() or (k4i >= NPW).any() ): continue
+                    k4  = kGRID[k4i[0],k4i[1],k4i[2],:]
+                    dot = np.dot( k2 - k4, k2 - k4 )
+                    if ( dot == 0.0 ): continue
+                
+                    FACTOR_EXCH = 4 * np.pi / dot
+                    D34 = get_1RDM( C, k3i[0] * NPW**2 + k3i[1] * NPW + k3i[2], k4i[0] * NPW**2 + k4i[1] * NPW + k4i[2] )
+                    V_ee_K += FACTOR_EXCH * D34
+                    
+    return V_ee_J - 0.5 * V_ee_K
 
 def get_V_ee_SLOW( k1, k2, C, kGRID ):
     """
@@ -146,28 +129,25 @@ def get_V_ee_SLOW( k1, k2, C, kGRID ):
     """
     ### Coulomb Term ###
     V_ee_J = 0.0 + 0.0j
-    for k4i, k4 in enumerate(kGRID):
-        for k3i, k3 in enumerate(kGRID):
-            if ( k3 - k4 == k2 - k1 ).all() & ( k2 - k1 != 0.0 ).all():
-                D34 = get_1RDM( C[:,:n_elec_alpha], k3i, k4i )
-                J   = 4 * np.pi / np.dot( k3 - k4, k3 - k4 )
-                V_ee_J += J * D34
+    dot = np.dot( k2 - k1, k2 - k1 )
+    if dot != 0.0:
+        one_over_r = 4 * np.pi / np.dot( k2 - k1, k2 - k1 )
+        for k4i, k4 in enumerate(kGRID):
+            for k3i, k3 in enumerate(kGRID):
+                if ( k3 - k4 == k2 - k1 ).all():
+                    D34 = get_1RDM( C[:,:n_elec_alpha], k3i, k4i )
+                    V_ee_J += D34 * one_over_r
     
     ### Exchange Term ###
     V_ee_K = 0.0 + 0.0j
     for k4i, k4 in enumerate(kGRID):
         for k3i, k3 in enumerate(kGRID):
-            if ( k3 - k1 == k2 - k4 ).all() & ( k4 - k2 != 0.0 ).all():
+            if ( k3 + k4 == k2 + k1 ).all() & ( np.dot( k4 - k2, k4 - k2 ) != 0.0 ).all():
                 D34 = get_1RDM( C[:,:n_elec_alpha], k3i, k4i )
                 K   = 4 * np.pi / np.dot( k4 - k2, k4 - k2 )
                 V_ee_K += K * D34  
     
-    
-    
     return V_ee_J - 0.500 * V_ee_K
-
-
-
 
 def get_V_NN( kGRID ):
     #RdotK = np.einsum("Rd,Kd->RK", coords[:,:] * kGRID[:,:])
@@ -194,20 +174,32 @@ def __get_FOCK( C, kGRID ):
     """
     FC = np.zeros( (NPW**3, n_elec_alpha), dtype=np.complex128 )
     for k1i, k1 in enumerate(kGRID):
+        T0 = time()
         print( "k1i: ", k1i, "of", len(kGRID) )
         T_e = get_T_e( k1 )
         for k2i in range(k1i+1,NPW**3):
             k2 = kGRID[k2i]
             #print( "k2i: ", k2i, "of", len(kGRID[k1i+1:]) )
+            
+            
             #V_NN = get_V_NN( k1, k2 )
-            V_eN = get_V_eN( k1, k2 )
-            V_ee = get_V_ee(k1i, k2i, k1, k2, C, kGRID )
-            if ( abs(V_ee) > 0.0  ):
-                print( V_ee, np.conj(get_V_ee( k2i, k1i, k2, k1, C, kGRID )) )
-                exit()
+            V_eN      = get_V_eN( k1, k2 )
+            V_ee      = get_V_ee(k1i, k2i, k1, k2, C, kGRID )
+            #V_ee_SLOW = get_V_ee_SLOW(k1, k2, C, kGRID )
+            #if ( abs(V_ee_SLOW) > 0.0  ):
+            #print( k1i, k2i )
+            #print( V_ee, np.conj(get_V_ee( k2i, k1i, k2, k1, C, kGRID )) )
+            #print( V_ee_SLOW, np.conj(get_V_ee_SLOW( k2, k1, C, kGRID )) )
+
+            #if (  np.abs(V_ee - V_ee_SLOW) > 1e-15  ):
+            #    print("Not equal", k1i, k2i, V_ee, V_ee_SLOW)
+            #    exit()
+
 
             tmp  = (T_e + V_eN + V_ee) * C[k2i,:]
             FC[k1i,:] += ( tmp + np.conj(tmp) ) # * 2 * 0.5 for double counting and average of .T
+        print("Time: ", time() - T0)
+    #exit()
     return FC
 
 def build_solve_C_Fock_C_matrix( C, kGRID ):
